ea2684fea89a957ea18d2004ae01adfe8e16d8e6
hkkim <hkkim0103@naver.com>
2019-03-12 21:57:37 +0900

    7th commit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '$w)8*k-@w^mv(a85&*5+8*y%npf2wt
 DEBUG = bool(os.environ.get('DJANGO_DEBUG',True))
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
